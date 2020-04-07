4743b723411ccd90b22129d2ed0a0d1349f1a16b
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:21:16 +0800

    init project

---
+++ ios/swift/KSmartEyeKit/KSmartEyeKit/KSmartEyeKit.h
@@ -0,0 +1,19 @@
+//
+//  KSmartEyeKit.h
+//  KSmartEyeKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSmartEyeKit.
+FOUNDATION_EXPORT double KSmartEyeKitVersionNumber;
+
+//! Project version string for KSmartEyeKit.
+FOUNDATION_EXPORT const unsigned char KSmartEyeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSmartEyeKit/PublicHeader.h>
+
+
